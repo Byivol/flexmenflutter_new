8928c6d463c7c5170f646bf6f4cffb54fdc72c0f
Марфенко Влад <vladmarfenko@gmail.com>
2024-05-17 08:22:31 +0300

    a

--- lib/Home.dart
+++ lib/Home.dart
@@ -146,7 +146,6 @@ class _HomeScreenState extends State<HomeScreen> {
                             ))
                       ],
                     ),
-                    ExpansionTileListView(scheduleItems: scheduleItems)
                   ],
                 ),
               ]),
