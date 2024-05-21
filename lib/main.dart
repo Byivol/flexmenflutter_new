b5b7e6fb1bd98d815d8f6d5a8befb643018ae72b
Florgus <81070602+Byivol@users.noreply.github.com>
2024-05-21 12:26:58 +0300

    Added2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
+
+import 'persistent_tab_view.dart';
 
 void main() {
   runApp(const MyApp());
