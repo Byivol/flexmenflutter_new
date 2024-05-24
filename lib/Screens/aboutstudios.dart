cc534e0afe7778ef6d9502d0aa83a24f120e68c8
Folly37 <follochka11@gmail.com>
2024-05-25 00:51:07 +0500

    update aboutStudios

--- lib/Screens/aboutstudios.dart
+++ lib/Screens/aboutstudios.dart
@@ -11,36 +11,86 @@ class _AboutStudiosState extends State<AboutStudios> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-  titleSpacing: -1,
-  backgroundColor: Colors.black,
-  foregroundColor: Colors.white,
-  title: const Text('О студии', style: TextStyle(fontSize: 20)),
-  leadingWidth: 100,
-  centerTitle: true,
-      ),
-      body: Container(
-      margin: const EdgeInsets.only(right: 16.0),
-      child: Row(
-        children: [
-          const SizedBox(width: 16.0),
-          Expanded(
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Поиск',
-                hintStyle: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
-                border: InputBorder.none,
-                contentPadding: const EdgeInsets.only(left: 16.0),
-                fillColor: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.1),
-                filled: true,
-                suffixIcon: const Icon(Icons.search, color: Color.fromARGB(255, 0, 0, 0)),
-              ),
-            ),
+  appBar: PreferredSize(
+  preferredSize: Size.fromHeight(120.0),
+  child: Stack(
+    children: [
+      AppBar(
+        titleSpacing: 25,
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        title: Padding(
+          padding: EdgeInsets.only(top: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('О студии', style: TextStyle(fontSize: 20),),
+              SizedBox(height: 2), 
+              Text('Тюмень', style: TextStyle(fontSize: 16, color: Color.fromARGB(255, 223, 223, 223)),),
+            ],
+          ),
+        ),
+        leadingWidth: 100,
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.format_list_bulleted, color: Colors.white),
+            onPressed: () {
+              
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.place_outlined),
+            onPressed: () {
+
+            },
           ),
         ],
       ),
-    ),
-    );
+      Positioned(
+        bottom: 15,
+        left: 20,
+        right: 20,
+        child: SizedBox(
+          width: 35, 
+          height: 35,
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: 'Поиск',
+              hintStyle: const TextStyle(color: Color.fromARGB(226, 0, 0, 0)),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              contentPadding: const EdgeInsets.only(left: 16.0),
+              fillColor: Color.fromARGB(255, 255, 255, 255),
+              filled: true,
+              suffixIcon: const Icon(Icons.search, color: Color.fromARGB(226, 0, 0, 0)),
+            ),
+          ),
+        ),   
+      ),
+    ],
+  ),
+),
 
+body: Padding(
+  padding: EdgeInsets.only(top: 15, left: 15),
+  child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text("THE FLEX MEN|Тюмень|Фабричная", style: TextStyle(fontSize: 18,fontWeight: FontWeight.w500),),
+      SizedBox(height:0), 
+      Text("Тюмень, ул.Фабричная, 9", style: TextStyle(fontSize: 13, color: Color.fromARGB(178, 0, 0, 0)),),
+      SizedBox(height: 2,),
+      Text('моя студия',style: TextStyle(fontSize: 13, color: Color.fromARGB(178, 0, 0, 0)),),
+      SizedBox(height: 1,),
+      Divider(),
+    ],
+  ),
+
+),
+
+    );
+    
   }
 }
